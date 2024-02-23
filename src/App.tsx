36b996e0fb10fad8220f86132b38946415861f0b
Tim DeCillis <timdecillis@gmail.com>
2024-02-23 12:43:17 -0800

    added login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,49 @@
 import "./App.css";
+import { useEffect } from "react";
+import { useAuth, useLoginWithRedirect, ContextHolder } from "@frontegg/react";
 
 function App() {
+  const { user, isAuthenticated } = useAuth();
+  const loginWithRedirect = useLoginWithRedirect();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      loginWithRedirect();
+    }
+  }, [isAuthenticated, loginWithRedirect]);
+
+  const logout = () => {
+    const baseUrl = ContextHolder.getContext().baseUrl;
+    window.location.href = `${baseUrl}/oauth/logout?post_logout_redirect_uri=${window.location}`;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Frontegg Assignment</h1>
-      </header>
+      {isAuthenticated ? (
+        <div>
+          <div>
+            <img
+              src={user?.profilePictureUrl ?? "../public/logo192.png"}
+              alt={user?.name}
+            />
+          </div>
+          <div>
+            <span>Logged in as: {user?.name}</span>
+          </div>
+          <div>
+            <button onClick={() => alert(user?.accessToken)}>
+              What is my access token?
+            </button>
+          </div>
+          <div>
+            <button onClick={() => logout()}>Click to logout</button>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <button onClick={() => loginWithRedirect()}>Click me to login</button>
+        </div>
+      )}
     </div>
   );
 }
