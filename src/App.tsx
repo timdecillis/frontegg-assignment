82afdbbd066f9d84f8e09d2ac560b4ea224f87c0
Tim DeCillis <timdecillis@gmail.com>
2024-03-13 16:09:41 -0700

    removed API request

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,14 @@ import {
 import "./App.css";
 import axios from "axios";
 
-function App() {
+const token = process.env.BEARER_TOKEN;
 
+function App() {
   let { user, isAuthenticated } = useAuth();
   const loginWithRedirect = useLoginWithRedirect();
   const { switchTenant } = useAuthActions();
 
-  const [APIName, setAPIName] = useState('');
+  const [APIName, setAPIName] = useState("");
 
   const handleSwitchTenant = (ID: string) => {
     switchTenant({ tenantId: ID });
@@ -42,8 +43,7 @@ function App() {
       method: "GET",
       url: "https://api.frontegg.com/identity/resources/vendor-only/users/v1/170d09a9-a6d3-4b29-a983-d889a754041d",
       headers: {
-        authorization:
-          "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsInRva2VuSXNzdWVyIjoiZnJvbnRlZ2cifQ.eyJzY29wZXMiOltdLCJ0eXBlIjoidmVuZG9yIiwidmVuZG9ySWQiOiIzNzJjYjc3ZC01MmVmLTQwZGMtOGY2Yy02YzZlMTI3YTY2OGUiLCJpYXQiOjE3MTAzNDk4NzcsImV4cCI6MTcxMDQzNjI3N30.az1mjmPyzRWz8xj0PAIrsqcPY9S-oZNxw0kGoQ9ALnIhvtnuNUcpdUPhyJcxlmuIknR99fyqXV5RQn0SdMXxaVq247kwy-ozPR_kYSpMuNt8kWkRtjDnTAeHUdGWogAVItOnjESK9SYDT5xRvXLGZIB8oVt4S_SkHmgIoURHkYwWHy2eqI9dD6JhoQm3Y1Zx5eWSHbZXNC801M68h7ZWUWFznOe71sO8q9AAsYYAMDamTgm0cYDE6_7oS-lZv-2HqD-fuzOmRoz9HW84EVOLgDWnnNMa0BtMxXjJ4cae9BJm3yVXHrzrjHAX6EUMMg1EjItNYKij4WY90q0KKxFp9A",
+        authorization: `Bearer ${token}`,
       },
     };
 
@@ -95,7 +95,6 @@ function App() {
               </select>
             </div>
           </div>
-
         </div>
       ) : (
         <div>
